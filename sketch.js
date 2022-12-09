7e700fd89fcc0aa5df4e3377f16afbb937d74785
vmana2 <vmana2@uic.edu>
2022-12-08 23:37:20 -0600

    Added car

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ var createScene = function () {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    var seaGull = new meshModel('./seagulf.glb', 0.9)
+    var car = new meshModel('./car.glb', 0.9)
 
     return scene;
 };
